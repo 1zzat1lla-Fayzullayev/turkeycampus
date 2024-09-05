a7b8005fffc660fd08a353a10450edfdc043686c
1zzat1lla-Fayzullayev <izzatillaf495@gmail.com>
2024-09-05 14:42:03 +0500

    change

--- src/components/services/services.jsx
+++ src/components/services/services.jsx
@@ -5,20 +5,16 @@ import ServicesCard from "./servicesCard";
 
 function Services() {
   return (
-    <>
-      <div>
-        <Wrapper>
-          <div className="mt-[150px] flex justify-center items-center flex-col">
-            <h2 className="text-[38px] font-[700] ">
-              Our <span className="text-[#e63f38]">Services</span>
-            </h2>
-            <div className="mt-[100px]">
-              <ServicesCard />
-            </div>
-          </div>
-        </Wrapper>
+    <Wrapper>
+      <div className="mt-[150px] flex justify-center items-center flex-col">
+        <h2 className="text-[38px] font-[700]">
+          Our <span className="text-[#e63f38]">Services</span>
+        </h2>
+        <div className="mt-[100px] w-full">
+          <ServicesCard />
+        </div>
       </div>
-    </>
+    </Wrapper>
   );
 }
 
--- src/components/services/servicesCard.jsx
+++ src/components/services/servicesCard.jsx
@@ -1,93 +1,53 @@
-/* eslint-disable react/jsx-key */
+/* eslint-disable react/prop-types */
 /* eslint-disable no-unused-vars */
-import React, { useRef } from "react";
-import { servicesData } from "../../data/servicesData";
-import Swiper from "./servicesSwiper";
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import { Navigation, Pagination, Autoplay } from "swiper/modules"; // Import necessary modules
+import { servicesData } from "../../data/servicesData"; // Import your services data
 
 function ServicesCard() {
-  
-
-  const swiperRef = useRef(null);
-
-  // Function to handle scrolling left
-  const scrollLeft = () => {
-    if (swiperRef.current) {
-      swiperRef.current.scrollBy({ left: -swiperRef.current.clientWidth, behavior: "smooth" });
-    }
-  };
-
-  // Function to handle scrolling right
-  const scrollRight = () => {
-    if (swiperRef.current) {
-      swiperRef.current.scrollBy({ left: swiperRef.current.clientWidth, behavior: "smooth" });
-    }
-  };
-
   return (
-    <div className="relative py-8 max-w-[85%] mx-auto">
-      {/* Buttons for navigation */}
-      {/* <button
-        onClick={scrollLeft}
-        className="absolute left-0 top-1/2 transform -translate-y-1/2 bg-[#1966a2] text-white p-2 rounded-full shadow-md z-20"
+    <div className="mt-[50px]">
+      <Swiper
+        modules={[Navigation, Pagination, Autoplay]}
+        spaceBetween={20}
+        slidesPerView={4}
+        // pagination={{ clickable: true }}
+        autoplay={{ delay: 3000, disableOnInteraction: false }}
+        breakpoints={{
+          320: {
+            slidesPerView: 1,
+            spaceBetween: 20,
+          },
+          640: {
+            slidesPerView: 2,
+            spaceBetween: 30,
+          },
+          1024: {
+            slidesPerView: 3,
+            spaceBetween: 30,
+          },
+          1280: {
+            slidesPerView: 4,
+            spaceBetween: 30,
+          },
+        }}
+        className="mt-10"
       >
-        &lt;
-      </button>
-      <button
-        onClick={scrollRight}
-        className="absolute right-0 top-1/2 transform -translate-y-1/2 bg-[#1966a2] text-white p-2 rounded-full shadow-md z-20"
-      >
-        &gt;
-      </button> */}
-
-      {/* Swiper Container */}
-      {/* <div
-        ref={swiperRef}
-        className="flex space-x-4 px-4"
-        style={{ scrollSnapType: "x mandatory" }}
-      >
-        {servicesData.map((item) => (
-          <div
-            key={item.title}
-            className="w-1/4 flex-shrink-0 relative z-[10] rounded-lg shadow-lg border border-gray-200 bg-white"
-            style={{ scrollSnapAlign: "start" }}
-          >
-            <div className="service-img flex items-center justify-center mt-4">
-              <img
-                src={item.pic}
-                alt="Service Icon"
-                className="h-24 w-24 rounded-full object-cover"
-              />
-            </div>
-            <div className="p-6 text-center">
-              <h4 className="text-xl font-bold mb-2">{item.title}</h4>
-              <p className="text-gray-600">{item.desc}</p>
-            </div>
-            <div className="flex justify-center mb-6">
-              <a
-                href="https://turkeycampus.com/en/contact-us"
-                className="px-8 py-2 border border-[#1966a2] text-[#1966a2] rounded-full hover:bg-[#1966a2] hover:text-white transition duration-300"
-              >
-                Contact Us
-              </a>
-            </div>
-          </div>
-        ))}
-      </div> */}
-      <Swiper autoplayInterval={4000}>
-        {/* <div className="bg-blue-500 h-64 flex justify-center items-center text-white text-2xl">Slide 1</div>
-        <div className="bg-red-500 h-64 flex justify-center items-center text-white text-2xl">Slide 2</div>
-        <div className="bg-green-500 h-64 flex justify-center items-center text-white text-2xl">Slide 3</div> */}
-        {servicesData.map((item) => (
+        {servicesData.map((item, index) => (
+          <SwiperSlide key={index}>
             <div
               key={item.title}
               className="relative select-none z-[10] w-[312px] rounded-lg min-h-[291px] mt-[65px] shadow-lg border border-gray-200 bg-white"
-              // style={{ scrollSnapAlign: "start" }}
             >
               <div className="service-img flex items-center justify-center mt-4">
                 <img
                   src={item.pic}
                   alt="Service Icon"
-                  className="h-24 w-24 rounded-full object-cover"
+                  className="h-[82px] w-[82px] rounded-full object-cover"
                 />
               </div>
               <div className="p-6 text-center">
@@ -103,7 +63,8 @@ function ServicesCard() {
                 </a>
               </div>
             </div>
-          ))}
+          </SwiperSlide>
+        ))}
       </Swiper>
     </div>
   );
